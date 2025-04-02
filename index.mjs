0ae0c80e2ca31da3b44d7f7863f3b1addb6b01ce
sumit1642 <sumeetyadav1642@gmail.com>
2025-04-02 20:22:13 +0530

    Fix: Removed Mockusers

--- index.mjs
+++ index.mjs
@@ -7,14 +7,6 @@ const PORT = process.env.PORT || 8000;
 
 app.use(router);
 
-const mockUsers = [
-	{ id: 1, username: "anson", displayName: "Anson" },
-	{ id: 2, username: "jack", displayName: "Jack" },
-	{ id: 3, username: "adam", displayName: "Adam" },
-	{ id: 4, username: "tina", displayName: "Tina" },
-	{ id: 5, username: "jason", displayName: "Jason" },
-	{ id: 6, username: "henry", displayName: "Henry" },
-	{ id: 7, username: "marilyn", displayName: "Marilyn" },
-];
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
