1c0beb0add1efd1905356f75a69103550c816f88
sumit1642 <sumeetyadav1642@gmail.com>
2025-04-06 14:22:45 +0530

    used cookie-parser as a middleWare

--- index.mjs
+++ index.mjs
@@ -1,10 +1,16 @@
 import express from "express";
 import { mainRoutes } from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+// cookie-parser parses incoming Cookie headers into req.cookies (and req.signedCookies if used with a secret)
+app.use(cookieParser()); // 🔥 Must be called to activate parsing
 app.use(mainRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
+
