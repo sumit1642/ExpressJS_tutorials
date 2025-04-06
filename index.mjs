2dd305b906d0a62a938c88cd1f293a145b1211ea
sumit1642 <sumeetyadav1642@gmail.com>
2025-04-06 16:24:41 +0530

    fix: set up cookie-parser middleware with secret key
    - Added cookie-parser middleware with 'testingSecret' to start parsing cookies.
    - This allows the app to read cookies and check signed cookies properly.

--- index.mjs
+++ index.mjs
@@ -7,10 +7,9 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 // cookie-parser parses incoming Cookie headers into req.cookies (and req.signedCookies if used with a secret)
-app.use(cookieParser()); // 🔥 Must be called to activate parsing
+app.use(cookieParser("testingSeceret")); // 🔥 Must be called to activate parsing
 app.use(mainRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
-
