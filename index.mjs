e83293eb9d2e57aff0d28c29f74eaf589e6d42f8
sumit1642 <sumeetyadav1642@gmail.com>
2025-04-07 13:27:12 +0530

    Added express-session to manage user sessions
    Set it up with:
    - a secret key to sign session IDs
    - saveUninitialized: false so we don’t store empty sessions
    - resave: false to avoid saving unchanged sessions
    - session cookie expires in 1 hour (60000 * 60 ms)
    
    This keeps users logged in for a bit and avoids unnecessary session saves.

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import { mainRoutes } from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -8,6 +9,20 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 // cookie-parser parses incoming Cookie headers into req.cookies (and req.signedCookies if used with a secret)
 app.use(cookieParser("testingSeceret")); // 🔥 Must be called to activate parsing
+
+/** NOTE: Always implement session before routes or routemiddleWare like mainRoutes */
+app.use(
+	session({
+		secret: "sumit-secret-key",
+		saveUninitialized: false,
+		resave: false,
+		cookie: {
+			// 60000 ms = 60seconds
+			maxAge: 60000 * 60, // 1hr
+		},
+	}),
+);
+
 app.use(mainRoutes);
 
 app.listen(PORT, () => {
